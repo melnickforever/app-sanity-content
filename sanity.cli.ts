66e50446a68759fb8ed96e05abaaf513a2704bc9
Dmytro Melnyk <dmytro.melnyk@vaimo.com>
2026-02-09 12:25:26 +0200

    Configuration: check git workflow

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -7,7 +7,7 @@ export default defineCliConfig({
   },
   deployment: {
     /**
-     * Enable auto-updates for studios.
+     * Enable auto-updates for studios. test
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
