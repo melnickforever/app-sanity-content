0974093a2b13640a4aa39d0c03f3a44e473b961d
Dmytro Melnyk <dmytro.melnyk@vaimo.com>
2026-02-09 12:16:48 +0200

    Configuration: add new configuration.

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   name: 'default',
   title: 'DmytroMelnyk',
 
-  projectId: 'a9jt0fhs',
-  dataset: 'production',
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
+  dataset: process.env.SANITY_STUDIO_DATASET!,
 
   plugins: [structureTool(), visionTool()],
 
