f47658616dfa1e3953921ae0c5d2e82c7449cafe
Dmytro Melnyk <dmytro.melnyk@vaimo.com>
2026-02-16 13:46:17 +0200

    Content: Portfolio

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,3 +1,5 @@
 import {pageType} from './pageType'
 import {skillType} from './skillType'
-export const schemaTypes = [pageType, skillType]
+import {portfolioType} from './portfolioType'
+
+export const schemaTypes = [pageType, skillType, portfolioType]
--- structure/index.ts
+++ structure/index.ts
@@ -6,17 +6,43 @@ export const structure: StructureResolver = (S) =>
     .items([
       S.divider().title('cms pages'),
       S.listItem()
-        .title('All')
+        .title('All Pages')
         .schemaType('page')
         .child(S.documentList().title('All CMS Pages').filter('_type == "page"')),
       S.listItem()
-        .title('Active')
+        .title('Active Pages')
         .schemaType('page')
-        .child(S.documentList().title('Active CMS Pages').filter('enabled == true')),
+        .child(S.documentList().title('CMS Pages').filter('_type == "page" && enabled == true')),
       S.listItem()
-        .title('Inactive')
+        .title('Inactive Pages')
         .schemaType('page')
-        .child(S.documentList().title('Inactive CMS Pages').filter('enabled == false')),
+        .child(S.documentList().title('CMS Pages').filter('_type == "page" && enabled == false')),
       S.divider().title('Portfolio'),
+      S.listItem()
+        .title('All Projects')
+        .schemaType('portfolio')
+        .child(
+          S.documentList()
+            .title('All CMS Pages')
+            .filter('_type == "portfolio"')
+            .defaultOrdering([{field: 'sortOrder', direction: 'asc'}]),
+        ),
+      S.listItem()
+        .title('Projects Enabled')
+        .schemaType('portfolio')
+        .child(
+          S.documentList().title('Projects')
+            .filter(' _type == "portfolio" && enabled == true')
+            .defaultOrdering([{field: 'sortOrder', direction: 'asc'}]),
+        ),
+      S.listItem()
+        .title('Projects Disabled')
+        .schemaType('portfolio')
+        .child(
+          S.documentList()
+            .title('Projects')
+            .filter(' _type == "portfolio" && enabled == false')
+            .defaultOrdering([{field: 'sortOrder', direction: 'asc'}]),
+        ),
       S.documentTypeListItem('skill').title('Skills'),
     ])
